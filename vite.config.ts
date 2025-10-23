5759ad2039f3c1e7675de2558057693ad49b52c2
Alexander Dullnig <alexander.dullnig@mait.at>
2025-10-23 13:58:36 +0200

    build: publish to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // Bei User-Seite am Root-Domain KEIN Subpfad nötig:
+    base: '/',
+    build: { outDir: 'docs' }
 })

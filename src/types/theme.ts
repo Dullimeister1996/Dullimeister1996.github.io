871f9e6f908bb60c83fec0b8840af3fb157e7196
Alexander Dullnig <alexander.dullnig@mait.at>
2026-05-28 11:19:43 +0200

    routefinderapp

--- src/types/theme.ts
+++
@@ -1,1 +0,0 @@
-export type Theme = 'light' | 'dark' | 'ocean' | 'sunset' | 'beer';
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    plugins: [react()],
-    // Bei User-Seite am Root-Domain KEIN Subpfad nötig:
-    base: '/',
-    build: { outDir: 'docs' }
-})
